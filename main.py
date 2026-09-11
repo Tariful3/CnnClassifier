10a9cd8291f13d878d9021b476b915a5c230c5f5
Tariful Islam <tarifulsarkar009@gmail.com>
2026-09-11 11:22:38 +0600

    utlis , exception and logger added

---
+++ main.py
@@ -0,0 +1,4 @@
+from CnnClasifier import logger
+
+logger.info("welcome to CnnClassifier")
+
--- template.py
+++ template.py
@@ -20,7 +20,7 @@ list_of_files = [
     f"src/{project_name}/constants/__init__.py",
 
     "config/config.yaml",
-    "dvc.yaml"
+    "dvc.yaml",
     "params.yaml",
     "requirements.txt",
     "setup.py",
